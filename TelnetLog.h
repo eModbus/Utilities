f196e67a8c00b0b04a8b0297499bdc00d92e6e5f
Miq1 <miq1@gmx.de>
2021-02-17 18:37:17 +0100

    Add (Telnet) logging

---
+++ TelnetLog.h
@@ -0,0 +1,35 @@
+// =================================================================================================
+// eModbus: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to eModbus
+//               MIT license - see license.md for details
+// =================================================================================================
+
+// Include Arduino.h to make Print and Serial known
+#include <Arduino.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#endif
+#include <WiFiUdp.h>
+
+class TelnetLog : public Print {
+public:
+  TelnetLog(uint16_t port, uint8_t maxClients);
+  ~TelnetLog();
+  void begin(const char *label);
+  void end();
+  void update();
+  inline bool isActive() { return telnetActive; };
+  size_t write(uint8_t c);
+  size_t write(const uint8_t *buffer, size_t size);
+
+protected:
+    // Telnet definitions
+    bool TL_ConnectionEstablished; // Flag for successfully handled connection
+    uint8_t TL_maxClients;
+    WiFiServer *TL_Server;
+    WiFiClient *TL_Client;
+    bool telnetActive;
+    char myLabel[64];
+};
+
